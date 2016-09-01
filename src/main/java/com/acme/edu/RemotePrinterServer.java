da89eb1f6074dd0f52a7792dbb17fcb72269596b
Nadya <db.javaschool@gmail.com>
2016-09-01 12:39:11 +0300

    Add multiply access

--- src/main/java/com/acme/edu/RemotePrinterServer.java
+++ src/main/java/com/acme/edu/RemotePrinterServer.java
@@ -6,11 +6,16 @@ import java.net.Socket;
 
 public class RemotePrinterServer {
     public static void main(String[] args) {
+        int maxClientNumber = 10000;
+        int curClientNumber = 0;
         try (
             ServerSocket serverSocket = new ServerSocket(1111))
             {
-                Socket client = serverSocket.accept();
-                SessionHandler sessionHandler = new SessionHandler(client);
+                while (curClientNumber < maxClientNumber) {
+                    curClientNumber++;
+                    Socket client = serverSocket.accept();
+                    SessionHandler sessionHandler = new SessionHandler(client);
+                }
         } catch (IOException e) {
             e.printStackTrace();
         }
