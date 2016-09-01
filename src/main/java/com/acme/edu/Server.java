a2de2a5cad8b439d6423910291985ed14628f3a9
Nadya <db.javaschool@gmail.com>
2016-09-01 18:15:37 +0300

    Send messages to all clients

--- src/main/java/com/acme/edu/Server.java
+++ src/main/java/com/acme/edu/Server.java
@@ -3,6 +3,7 @@ package com.acme.edu;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -13,7 +14,11 @@ public class Server {
             Collections.synchronizedList(new ArrayList<SessionHandler>());
 
     public static void main(String[] args) {
-        int maxClientNumber = 10000;
+        Server server = new Server();
+    }
+
+    private Server() {
+        final int maxClientNumber = 10000;
         int curClientNumber = 0;
         try (
                 ServerSocket serverSocket = new ServerSocket(1111))
@@ -27,6 +32,72 @@ public class Server {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private class SessionHandler extends Thread {
+        private Socket socket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public SessionHandler(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new BufferedReader(
+                        new InputStreamReader(
+                                new BufferedInputStream(
+                                        socket.getInputStream()
+                                )
+                        )
+                );
+                out = new PrintWriter(
+                        new BufferedWriter(
+                                new OutputStreamWriter(
+                                        new BufferedOutputStream(
+                                                socket.getOutputStream()
+                                        )
+                                )
+                        )
+                );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            while (true) {
+                try {
+                    String rcv_msg = in.readLine();
+                    if (rcv_msg == null) break;
+
+                    String msg = rcv_msg + " [" + LocalDateTime.now() + "]";
+                    System.out.println(msg);
+                    for (SessionHandler sessionHandler:sessionHandlerList) {
+                        sessionHandler.send(msg);
+                    }
+                } catch (IOException e) {
+                    break;
+                }
+            }
+
+            close();
+        }
+
+        public synchronized void send(String msg) {
+            out.println(msg);
+            out.flush();
+        }
+
+        void close() {
+            System.out.println("Close connection");
+            System.out.println();
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
