012f0f5acee83d36fadfd50d1b3f3c87e78858fb
Nadya <db.javaschool@gmail.com>
2016-09-02 12:55:22 +0300

    Fix monitor

--- src/main/java/com/acme/edu/Server.java
+++ src/main/java/com/acme/edu/Server.java
@@ -3,26 +3,23 @@ package com.acme.edu;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static javafx.application.Platform.exit;
+
 class Server {
+    private final int PORT = 1111;
     private final Set<SessionHandler> sessionHandlerSet =
             Collections.synchronizedSet(new HashSet<SessionHandler>());
     private ServerSocket serverSocket;
     private volatile boolean running = true;
 
-    private Server() {
-        try {
-            serverSocket = new ServerSocket(1111);
-        } catch (IOException e) {
-            System.err.println("/Cannot start service");
-        }
+    private Server() throws IOException {
+        serverSocket = new ServerSocket(PORT);
     }
 
     /**
@@ -30,7 +27,13 @@ class Server {
      * @param args Command line arguments.
      */
     public static void main(String[] args) {
-        new Server().runServer();
+        try {
+            new Server().runServer();
+        } catch (NullPointerException | IOException e) {
+            System.err.println("/Cannot start service");
+            System.err.println("/Exit");
+            exit();
+        }
     }
 
     private void runServer() {
@@ -60,7 +63,7 @@ class Server {
             curClientNumber++;
             try {
                 Socket client = serverSocket.accept();
-                SessionHandler sessionHandler = new SessionHandler(client);
+                SessionHandler sessionHandler = new SessionHandler(client, sessionHandlerSet);
                 sessionHandlerSet.add(sessionHandler);
                 sessionHandler.start();
             } catch (IOException e) {
@@ -75,74 +78,6 @@ class Server {
             serverSocket.close();
         } catch (IOException e) {
             System.err.println("/Cannot finish server");
-        }
-    }
-
-    private class SessionHandler extends Thread {
-        private final Socket socket;
-        private PrintWriter out;
-        private BufferedReader in;
-
-        SessionHandler(Socket socket) {
-            this.socket = socket;
-            try {
-                in = new BufferedReader(
-                        new InputStreamReader(
-                                new BufferedInputStream(
-                                        socket.getInputStream()
-                                ), StandardCharsets.UTF_8
-                        )
-                );
-                out = new PrintWriter(
-                        new BufferedWriter(
-                                new OutputStreamWriter(
-                                        new BufferedOutputStream(
-                                                socket.getOutputStream()
-                                        ), StandardCharsets.UTF_8
-                                )
-                        )
-                );
-            } catch (IOException e) {
-                System.err.println("/Cannot open client socket");
-            }
-        }
-
-        @Override
-        public void run() {
-            String msg;
-            try {
-                while (true) {
-                    msg = in.readLine();
-                    if (msg == null) {
-                        break;
-                    }
-                    send(msg);
-                }
-            } catch (IOException ignored) {}
-
-            close();
-        }
-
-        private synchronized void send(String msg) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("[HH:mm:ss dd.MM.yyyy] ");
-            String sndMsg = dateFormat.format(new Date()) + msg;
-            for (SessionHandler sessionHandler:sessionHandlerSet) {
-                sessionHandler.out.println(sndMsg);
-                sessionHandler.out.flush();
-            }
-            System.out.println(sndMsg);
-        }
-
-        private void close() {
-            System.err.println("/Close connection");
-            sessionHandlerSet.remove(this);
-            try {
-                in.close();
-                out.close();
-                socket.close();
-            } catch (IOException e) {
-                System.err.println("/Cannot close client connection");
-            }
         }
     }
 }
---
+++ src/main/java/com/acme/edu/SessionHandler.java
@@ -0,0 +1,78 @@
+package com.acme.edu;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
+
+class SessionHandler extends Thread {
+    private final Set<SessionHandler> sessionHandlerSet;
+    private final Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    SessionHandler(Socket socket, Set<SessionHandler> sessionHandlerSet) {
+        this.sessionHandlerSet = sessionHandlerSet;
+        this.socket = socket;
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(
+                            new BufferedInputStream(
+                                    socket.getInputStream()
+                            ), StandardCharsets.UTF_8
+                    )
+            );
+            out = new PrintWriter(
+                    new BufferedWriter(
+                            new OutputStreamWriter(
+                                    new BufferedOutputStream(
+                                            socket.getOutputStream()
+                                    ), StandardCharsets.UTF_8
+                            )
+                    )
+            );
+        } catch (IOException e) {
+            System.err.println("/Cannot open client socket");
+        }
+    }
+
+    @Override
+    public void run() {
+        String msg;
+        try {
+            while (true) {
+                msg = in.readLine();
+                if (msg == null) {
+                    break;
+                }
+                send(msg);
+            }
+        } catch (IOException ignored) {}
+
+        close();
+    }
+
+    private synchronized void send(String msg) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("[HH:mm:ss dd.MM.yyyy] ");
+        String sndMsg = dateFormat.format(new Date()) + msg;
+        for (SessionHandler sessionHandler:sessionHandlerSet) {
+            sessionHandler.out.println(sndMsg);
+            sessionHandler.out.flush();
+        }
+        System.out.println(sndMsg);
+    }
+
+    void close() {
+        System.err.println("/Close connection");
+        sessionHandlerSet.remove(this);
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("/Cannot close client connection");
+        }
+    }
+}
